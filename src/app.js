6a1f58e65e50401c8155fe17ae6289764e393ee9
Netanel Wallach <netanelwallach@gmail.com>
2026-06-04 23:21:09 +0300

    JS-Recursion-Ex2

--- src/app.js
+++ src/app.js
@@ -14,9 +14,26 @@ const findFactorial = function (num) {
 };
 
 //Exercise 2
-const reverseString = function () {
+const reverseString = function (str) {
   //Your code here
+  let size = str.length;
+  if (str === "") {
+    return str;
+  }
+  let reverse = str.charAt(size - 1);
+  return reverse + reverseString(str.slice(0, -1));
 };
+
+// const reverseString = function (str, left = 0, right = str.length - 1) {
+//   if (left >= right) {
+//     return left === right ? str.charAt(left) : "";
+//   }
+
+//   let firstChar = str.charAt(left);
+//   let lastChar = str.charAt(right);
+
+//   return lastChar + reverseString(str, left + 1, right - 1) + firstChar;
+// };
 
 //Exercise 3
 const arr1 = [1, 2, 3];
