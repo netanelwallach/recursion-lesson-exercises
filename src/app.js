52660194e3e43efaaaa14ef99dd294730f93e94b
Netanel Wallach <netanelwallach@gmail.com>
2026-06-05 09:21:30 +0300

    JS-Recursion-Extension

--- src/app.js
+++ src/app.js
@@ -39,13 +39,23 @@ const reverseString = function (str) {
 const arr1 = [1, 2, 3];
 const arr2 = [];
 
-const swap = function (arr1, arr2) {
+// const swap = function (arr1, arr2) {
+//   //Your code here
+//   if (arr1.length === 0) {
+//     return;
+//   }
+//   arr2.push(arr1.shift());
+//   swap(arr1, arr2);
+// };
+
+const swap = function (arr1, arr2, arr3 = []) {
   //Your code here
-  if (arr1.length === 0) {
+  if (arr1.length === 0 && arr3.length === 0) {
     return;
   }
-  arr2.push(arr1.shift());
+  arr3.push(arr1.pop());
   swap(arr1, arr2);
+  arr2.push(arr3.pop());
 };
 
 /* DO NOT REMOVE THE EXPORTS BELOW */
